86e57ff6ebd92164046c28b5ee62a08d977ee0eb
Barnabas27 <bnyambura032@gmail.com>
2020-11-08 22:59:08 +0300

    import instance of quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,7 @@ export class QuoteComponent implements OnInit {
   quotes: Quotes [] = [
     new Quotes(1,"noman is an island", "reggea",new Date(12, 8, 2020)),
     new Quotes(2, "If you behave abnomary.", "kenyatta", new Date(12, 8, 2020)),
-    new Quotes(3, "hey", "barbz", new Date(12, 8, 2020))
+    new Quotes(3, "hey", "barbz", new Date(12, , 2020))
   ];
 
   constructor() { }
