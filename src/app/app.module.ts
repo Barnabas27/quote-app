88c4566b55200733b91a87c1efd4bbe7e915cff7
Barnabas27 <bnyambura032@gmail.com>
2020-11-08 22:16:59 +0300

    add mapping to quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailsComponent } from './quote/quote-details/quote-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
+    QuoteFormComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quotes } from '../quotes';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  
+  quotes: Quotes [] = [
+    new Quotes(1,"noman is an island", "reggea",new Date(12, 8, 2020)),
+    new Quotes(2, "If you behave abnomary.", "kenyatta", new Date(12, 8, 2020)),
+    new Quotes(3, "hey", "barbz", new Date(12, 8, 2020))
+  ];
 
   constructor() { }
 
